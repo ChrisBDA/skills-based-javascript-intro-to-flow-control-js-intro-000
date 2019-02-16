9792cc72e4f1ed81446d079fcb722cf1941f19c2
Christopher Light <light.christopher@gmail.com>
2019-02-16 15:11:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,16 +1,16 @@
 function basicTeenager(age) {
-  
+
 if (age >= 13 && age <= 19) {
-  
+
 return "You are a teenager!"
 
 }
 }
 
 function teenager(age) {
-  
+
 if (age >= 13 && age <= 19) {
-  
+
 return "You are a teenager!" }
 
 else {return "You are not a teenager" }
@@ -18,13 +18,13 @@ else {return "You are not a teenager" }
 }
 
 function ageChecker(age) {
-  
+
 if (age >= 13 && age <= 19) {
-  
+
 return "You are a teenager!" }
 
 else if (age <= 12) {
-  
+
 return "You are a kid" }
 
 else {return "You are a grownup" }
